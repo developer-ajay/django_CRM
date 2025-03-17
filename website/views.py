ba15935ca5eed208c9923cc8a6ed28351de3e9d3
developer-ajay <developer.ajay618@gmail.com>
2025-03-17 07:49:59 +0530

    login user updates

--- website/views.py
+++ website/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request , 'home.html', {})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.error(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    
+    return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
